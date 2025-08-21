e1164a683a6480617f3033748d236340b754a97b
SujalSourav <19winsujal@gmail.com>
2025-08-21 23:49:52 +0530

    new commit

--- miniproject03/src/Components/Card.js
+++ miniproject03/src/Components/Card.js
@@ -36,7 +36,7 @@ const Card = (props) => {
   return (
     <div className="w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden">
       <div className="relative">
-        <img src={course.image.url} />;
+        <img src={course.image.url} />
         <div
           className="w-[40px] h-[40px] bg-white rounded-full absolute right-2 
         bottom-3 grid place-items-center"
